b110ea9883b2497d0dc65c0d9e7c202cc3bf1f2c
Devon Burriss <devon@chimplab.co>
2016-06-06 23:13:30 +0200

    cleanup

--- SimpleInjector.Microsoft.Extensions.Configuration/SimpleInjectorOptionsExtension.cs
+++ SimpleInjector.Microsoft.Extensions.Configuration/SimpleInjectorOptionsExtension.cs
@@ -7,36 +7,6 @@ namespace SimpleInjector.Microsoft.Extensions.Configuration
 {
     public static class SimpleInjectorOptionsExtension
     {
-        public static void AddSettings<T>(this Container container, IConfiguration configuration) where T : class, new()
-        {
-
-            container.ResolveUnregisteredType += (sender, e) =>
-            {
-                if (e.UnregisteredServiceType.IsGenericType &&
-                    e.UnregisteredServiceType.GetGenericTypeDefinition() == typeof(IOptions<>))
-                {
-                    var argType = e.UnregisteredServiceType.GenericTypeArguments.First();
-                    var optionsType = typeof(IOptions<>).MakeGenericType(argType);
-
-                    var optionValue = Activator.CreateInstance<T>();
-                    PopulateSettingObject(configuration, argType, optionValue);
-                    var optionsObj = Options.Create(optionValue);
-                    e.Register(() => optionsObj);
-                }
-            };
-        }
-
-        private static void PopulateSettingObject<T>(IConfiguration configuration, Type argType, T optionValue) where T : class, new()
-        {
-            var props = argType.GetProperties();
-            foreach (var p in props)
-            {
-                var name = p.Name;
-                var value = configuration[name];
-                p.SetValue(optionValue, value);
-            }
-        }
-
         public static void AddOptions(this Container container, IConfiguration configuration)
         {
             container.ResolveUnregisteredType += (sender, e) =>
@@ -57,7 +27,36 @@ namespace SimpleInjector.Microsoft.Extensions.Configuration
                 }
             };
         }
+
+        public static void AddSettings<T>(this Container container, IConfiguration configuration) where T : class, new()
+        {
+            container.ResolveUnregisteredType += (sender, e) =>
+            {
+                if (e.UnregisteredServiceType.IsGenericType &&
+                    e.UnregisteredServiceType.GetGenericTypeDefinition() == typeof(IOptions<>))
+                {
+                    var argType = e.UnregisteredServiceType.GenericTypeArguments.First();
+                    var optionsType = typeof(IOptions<>).MakeGenericType(argType);
+
+                    var optionValue = Activator.CreateInstance<T>();
+                    PopulateSettingObject(configuration, argType, optionValue);
+                    var optionsObj = Options.Create(optionValue);
+                    e.Register(() => optionsObj);
+                }
+            };
+        }
+
+
+        private static void PopulateSettingObject<T>(IConfiguration configuration, Type argType, T optionValue) where T : class, new()
+        {
+            var props = argType.GetProperties();
+            foreach (var p in props)
+            {
+                var name = p.Name;
+                var value = configuration[name];
+                p.SetValue(optionValue, value);
+            }
+        }
+
     }
-
-
 }
